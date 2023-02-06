6e6ab36aa98c9986fba53118eb90b575719c7c28
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-06 17:06:45 +0100

    some css and comments on variables

--- badword.php
+++ badword.php
@@ -1,8 +1,13 @@
 <?php
+// PAROLA CENSURATA
 $bad_word = $_GET['badword'];
+// PARAGRAFO ORIGINALE
 $paragraph = $_GET['paragraph'];
-$paragraph_censured = str_replace($bad_word, '****',$paragraph );
+// PARAGRAFO ORIGINALE LENGTH
 $paragraph_length = strlen($paragraph);
+// PARAGRAFO CENSURATO
+$paragraph_censured = str_replace($bad_word, '****',$paragraph );
+// PARAGRAFO CENSURATO LENGTH
 $paragraph_censured_length = strlen($paragraph_censured);
 ?>
 
@@ -13,18 +18,24 @@ $paragraph_censured_length = strlen($paragraph_censured);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>badword_result</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <!-- ORIGINAL RESULT -->
-    <div class="container-fluid">
-        <h4>Paragrafo originale</h4>
-        <p><?php echo $paragraph ?></p>
-        <p class="fst-italic">Il Paragrafo originale è lungo <?php echo $paragraph_length ?> caratteri.</p>
-        <hr>
-    <!-- CENSURED RESULT -->
-    <h4>Paragrafo censurato</h4>
-        <p><?php echo $paragraph_censured ?></p>
-        <p class="fst-italic">Il Paragrafo censurato è lungo <?php echo $paragraph_censured_length ?>caratteri.</p>
+    <div class="container-fluid text-center d-flex flex-column align-items-center">
+        <!-- ORIGINAL RESULT -->
+        <div class="card w-50 m-3 p-3">
+            <h4 class="text-primary">Paragrafo originale</h4>
+            <p><?php echo $paragraph ?></p>
+            <p class="fst-italic">Il Paragrafo originale è lungo <?php echo $paragraph_length ?> caratteri.</p>
+        </div>
+        <!-- CENSURED RESULT -->
+       <div class="card w-50 m-3 p-3">
+            <h4 class="text-danger">Paragrafo censurato</h4>
+            <p><?php echo $paragraph_censured ?></p>
+            <p class="fst-italic">Il Paragrafo censurato è lungo <?php echo $paragraph_censured_length ?>caratteri.</p>
+       </div>
     </div>
 </body>
 </html>
